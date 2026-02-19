26a630c170ba472df653b723d612da5772225b7e
kaziErfan <erfanhtamim1@gmail.com>
2026-02-19 23:53:38 +0600

    footer added

--- script/index.js
+++ script/index.js
@@ -9,19 +9,10 @@ const loadAllProduct = () => {
 const displayTrending = (products) => {
   const trendingProduct = document.getElementById("trending-product");
   trendingProduct.innerHTML = "";
-// //   "id": 10,
-// // "title": "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
-// // "price": 109,
-// // "description": "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
-// // "category": "electronics",
-// // "image": "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_t.png",
-// // "rating": {
-// // "rate": 2.9,
-// "count": 470
 
+// Trending Product Loop
 products.forEach((product) => {
     if(product.price >= 550){
-        console.log(product.title);
          const productDiv = document.createElement("div");
          productDiv.innerHTML = `
         <div class="card bg-base-100 lg:w-96 w-11/12 mx-auto shadow-sm border-1 border-gray-200">
